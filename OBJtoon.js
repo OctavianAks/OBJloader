41ef82c44b6aa616f56e237d43250a133d336e0b
OctavianAks <107268063+OctavianAks@users.noreply.github.com>
2023-03-11 17:59:15 +0300

    Update OBJtoon.js

--- OBJtoon.js
+++ OBJtoon.js
@@ -22,9 +22,9 @@ function init() {
 
 
 	camera = new THREE.PerspectiveCamera( 25, window.innerWidth / window.innerHeight, 0.01, 10 );
-	camera.position.z = 3;
+	camera.position.z = 1;
      camera.position.y = 0.3 ;
-     camera.position.x = 1; 
+     camera.position.x = 2; 
 
 
 	// scene
